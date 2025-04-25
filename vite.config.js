02c8b2d007c3a4105124d5746abd847d9bcdf918
Luis Freites <luisfreites83@gmail.com>
2025-04-25 10:26:59 -0400

    vite.config changed

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { viteStaticCopy } from "vite-plugin-static-copy";
+import { resolve } from "path";
+import { fileURLToPath } from "url";
+import path from "path";
 
-// https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
-  plugins: [react({ fastRefresh: false })],
+  plugins: [
+    react({ fastRefresh: false }),
+    viteStaticCopy({
+      targets: [
+        {
+          src: resolve(__dirname, "public"),
+          dest: "",
+        },
+      ],
+    }),
+  ],
 });
